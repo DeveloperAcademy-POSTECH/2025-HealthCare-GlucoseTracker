fb949cc57f9873e6f4390e320d93c6facec9679d
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-09-02 22:39:01 +0900

    chore: 프로젝트 세팅

---
+++ GlucoseTracker/GlucoseTracker/Application/GlucoseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoseTrackerApp.swift
+//  GlucoseTracker
+//
+//  Created by taeni on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GlucoseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlucoseTracker/GlucoseTracker/Models/Glucose.swift
@@ -0,0 +1,7 @@
+//
+//  Glucose.swift
+//  GlucoseTracker
+//
+//  Created by taeni on 9/2/25.
+//
+
