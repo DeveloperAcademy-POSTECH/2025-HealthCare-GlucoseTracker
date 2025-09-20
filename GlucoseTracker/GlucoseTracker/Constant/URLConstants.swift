a0e172f1842c514e5655a5576e445d53814ce4be
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-09-20 13:18:40 +0900

    feat: 다크모드, 가로모드 차단

--- GlucoseTracker/GlucoseTracker/Constant/URLConstants.swift
+++ GlucoseTracker/GlucoseTracker/Constant/URLConstants.swift
@@ -9,4 +9,10 @@ import Foundation
 
 struct URLConstants {
     static let privacyURL = URL(string: "https://glucosetracker.notion.site/Glucose-tracker-26234b48a89480afa374d78df3824ced")!
+    
+    
+    static let glucoseDiabetesURL = URL(string: "https://diabetesjournals.org/care/article/48/Supplement_1/S128/157561/6-Glycemic-Goals-and-Hypoglycemia-Standards-of")!
+    
+    
+    static let supportURL = URL(string: "https://github.com/DeveloperAcademy-POSTECH/2025-HealthCare-GlucoseTracker/discussions/4")!
 }
